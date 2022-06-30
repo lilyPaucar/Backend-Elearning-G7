7ecad13a822a1fd6035d11a0ac9b65aad25942cb
lilyPaucar <lilipaucar03@gmail.com>
2022-06-29 21:26:01 -0500

    actualizar

--- Dia3/inicios-flask.py
+++ Dia3/inicios-flask.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from datetime import datetime
+from flask_cors import CORS
 app=Flask(__name__)
+
+CORS(app)
 
 @app.route('/')
 
 def rutaInicial():
     print('ingreso al enpoint inicial')
     return 'Bienvenido a tu primera API de CodiGo de Backend ;D'
+@app.route('/estado')
+def estadoAPI():
+    return{
+        'hora': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
 app.run(debug=True)

d6e86c02c9b122c4f9df18065188be50ec6b30f6
lilyPaucar <lilipaucar03@gmail.com>
2022-08-03 15:41:55 -0500

    semana06

---
+++ server.js
@@ -0,0 +1,68 @@
+// forma de usar las librerias usando ECMAScript
+import express from "express" ;
+import dotenv from "dotenv";
+// forma de usar las librerias usando CommonJs
+// const express = require("express");
+
+// Buscara el archivo .env y seteara las variables definidas en ese archivo como variables de entorno
+dotenv.config();
+
+const servidor =express();
+const PORT = process.env.PORT; // esto generalmente es 3000 PERO puede ser cualquier valor
+
+// indicamos que nuestro servidor podra aceptar y entender la informacion enviada en formato JSON
+servidor.use(express.json());
+
+const categories = [
+    {
+      name: "Zapatos",
+      description: "Zapatos para hombres, mujeres, niños y niñas",
+    },
+  ];
+
+servidor.get("/", (req, res) => {
+    res.status(200).json({
+      message: "Bienvenido a mi primera API",
+    });
+  });
+
+servidor
+  .route("/categories")
+  .get((req, res) => {
+    return res.status(200).json({ categories });
+  })
+  .post((req, res) => {
+    const category = req.body;
+
+    // agregara la nueva categoria al listado de categorias
+    categories.push(category);
+
+    return res.status(201).json({
+        message: "Category created successfully",
+        content: category,
+    });
+})
+
+servidor.route("/categories/:id").get((req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    // En base al id dado por la URL buscar en la lista de categorias si existe, si no existe indicar en el message que la categoria no existe, caso contrario devolver la categoria, NOTA: usar la posicion de la lista como el ID
+  
+    console.log(categories[id]);
+
+    if (categories[id] !== undefined) {
+        return res.json({
+          message: "La categoria es",
+          content: categories[id],
+        });
+      } else {
+        return res.status(400).json({
+          message: "La categoria no existe",
+          content: null,
+        });
+      }
+});
+
+servidor.listen(PORT,()=>{
+    console.log(`Servidor corriendo exitosamente en el puerto ${PORT}`)
+});
